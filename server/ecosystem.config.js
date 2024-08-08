9058b9567dbfce28aea0746a1eea47ad87af7a0a
Lauren Cavanaugh <laurencavanaugh@laurens-mbp-2.mynetworksettings.com>
2024-08-07 22:41:58 -0700

    modified ecosystem

--- server/ecosystem.config.js
+++ server/ecosystem.config.js
@@ -4,7 +4,10 @@ module.exports = {
             name: "proxy-server",
             script: "npm",
             args: "run start",
-            
+            env: {
+                NODE_ENV: "development",
+                ENV_VAR1: "environment-variable",
+            }
         }
     ]
 }
